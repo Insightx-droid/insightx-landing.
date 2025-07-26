1f01f95a7930fc8aa80e30f9491519c47f05ccf3
Insightx-droid <insightxlabfuture@gmail.com>
2025-07-26 16:24:38 +0530

    Update login.js

--- login.js
+++ login.js
@@ -4,12 +4,12 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   const email = document.getElementById("loginEmail").value;
   const password = document.getElementById("loginPassword").value;
 
-  fetch("https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec?email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password))
+  fetch("https://script.google.com/macros/s/AKfycbxVpB2rDKmAg5Xn8xzrReVQV8hOaRJ-n3NMJQT4RLXfVvsgcuD7eC27_mbAHsfXdb3r/exec?email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password))
     .then(res => res.json())
     .then(data => {
       if (data.success) {
         alert("Login successful!");
-        window.location.href = "student-lab.html";
+        window.location.href = "index.html";
       } else {
         alert("Invalid email or password.");
       }
