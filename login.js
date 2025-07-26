8a2244c5505ea6e23fb87d43f1cc121b927b7f82
Insightx-droid <insightxlabfuture@gmail.com>
2025-07-26 16:27:19 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const email = document.getElementById("loginEmail").value;
-  const password = document.getElementById("loginPassword").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   fetch("https://script.google.com/macros/s/AKfycbxVpB2rDKmAg5Xn8xzrReVQV8hOaRJ-n3NMJQT4RLXfVvsgcuD7eC27_mbAHsfXdb3r/exec?email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password))
     .then(res => res.json())
